72264e93119855e9f96ba3280e99afc9cd6b20d2
srivishnu-sivan <mallikasrivishnu@gmail.com>
2026-08-12 22:13:50 +0530

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,58 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type CreateVehicleRequest struct {
+	VehicleID   string `json:"vehicle_id" binding:"required"`
+	Description string `json:"description" binding:"required"`
+}
+
+func main() {
+gin.SetMode(gin.DebugMode)
+	router := gin.Default()
+
+	router.GET("/health", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{
+			"status": "ok",
+		})
+	})
+
+	router.GET("/vehicles/:id", func(c *gin.Context) {
+
+		c.JSON(200, gin.H{
+			"status":     "ok",
+			"vehicle_id": c.Param("id"),
+		})
+	})
+
+	router.GET("/vehicle", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status":         "ok",
+			"vehicle_id":     c.Query("id"),
+			"page_no":        c.Query("page"),
+			"vehicle_status": c.Query("status"),
+			"limit":          c.Query("limit"),
+		})
+	})
+
+	router.POST("/vehicles", func(c *gin.Context) {
+
+		var req CreateVehicleRequest
+
+		if err := c.ShouldBind(&req); err != nil {
+			c.JSON(400, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"vehicle_id":  req.VehicleID,
+			"description": req.Description,
+		})
+	})
+
+	router.Run(":8080")
+
+}
---
+++ internal/config/config.go
@@ -0,0 +1,21 @@
+package internal
+
+import (
+	"os"
+)
+
+type Config struct {
+	Port string
+}
+
+func Load() Config {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	return Config{
+		Port: port,
+	}
+}
